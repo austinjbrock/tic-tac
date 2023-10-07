7b1c012dc2871521c142a0674f5d74b6c59df956
austin brock <austinjbrock@gmail.com>
2023-10-06 21:01:01 -0700

    added array fill to board component

--- tic-tac/src/App.jsx
+++ tic-tac/src/App.jsx
@@ -2,50 +2,29 @@ import './App.css'
 import { useState } from 'react'
 
 
-function Square(){
-  
-  //add state:
-  const [value,setValue] = useState(null)
-
-
-  // this function will handle clicks for user input
-  function handleClick(){
-      setValue('X')
-  }
-
-
-
-  return (
-    <button
-     className='square' onClick={handleClick}>{value}</button>
-   )
+function Square({ value }) {
+  return <button className="square">{value}</button>;
 }
-
-
-
-
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
   return (
     <>
-    <div className='board-row'>
-      <Square />
-      <Square />
-      <Square />
-    </div>
-    <div className='board-row'>
-      <Square />
-      <Square />
-      <Square />
-    </div>
-    <div className='board-row'>
-      <Square />
-      <Square />
-      <Square />
-    </div>
+      <div className="board-row">
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
+      </div>
     </>
-  )
+  );
 }
-
-
-
